bf1530d2441d26b7b5c7b504843bfbbb0880fb75
guidoarnone <guidoar.96@gmail.com>
2020-04-18 16:59:53 -0300

    Agrego el zoom

---
+++ LD46/Assets/scripts/CameraManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+    private Camera camera;
+    public float maxCameraSize;
+    public float minCameraSize;
+    public float zoomSpeed;
+
+    void Start() {
+      camera = gameObject.GetComponent<Camera>();
+    }
+
+    void Update() {
+      updateZoom(Input.GetAxis("Mouse ScrollWheel"));
+      if(Input.GetKeyDown(KeyCode.Space)) {
+        center();
+      }
+      pan(Input.GetAxis("Mouse X"));
+    }
+
+    private void updateZoom(float scroll) {
+      float newCameraSize = camera.orthographicSize += scroll * Time.deltaTime * zoomSpeed;
+      if (newCameraSize > maxCameraSize) {
+        camera.orthographicSize = maxCameraSize;
+      } else if(newCameraSize < minCameraSize) {
+        camera.orthographicSize = minCameraSize;
+      } else {
+        camera.orthographicSize = newCameraSize;
+      }
+    }
+
+    private void center() {
+
+    }
+
+    private void pan(float panvalue) {
+    }
+
+}
