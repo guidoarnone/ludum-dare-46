56782898bd45dbc83e1f2af6015440240c144edd
Firewill <teo.kohan@gmail.com>
2020-04-20 22:08:53 -0300

    Publish

--- LD46/Assets/scripts/path/Path.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
-
-public abstract class Path : MonoBehaviour {
-    public abstract Vector3 getPosition(float t);
-}
